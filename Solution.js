63b489ba31216c5377498f219c3f9944f19c0a44
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-07-05 15:32:11 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,37 @@
+
+/**
+ * @param {string} moves
+ * @return {number}
+ */
+var maxDistance = function (moves) {
+    const UP = 'U';
+    const DOWN = 'D';
+    const LEFT = 'L';
+    const RIGHT = 'R';
+    const FREE_TO_CHOOSE_MOVE = '_';
+
+    let verticalDistance = 0;
+    let horizontalDistance = 0;
+    let numberOfFreeToChooseMoves = 0;
+
+    for (let move of moves) {
+
+        switch (move) {
+            case UP :
+                ++verticalDistance;
+                break;
+            case DOWN :
+                --verticalDistance;
+                break;
+            case LEFT :
+                --horizontalDistance;
+                break;
+            case RIGHT :
+                ++horizontalDistance;
+                break;
+            case FREE_TO_CHOOSE_MOVE :
+                ++numberOfFreeToChooseMoves;
+        }
+    }
+    return Math.abs(verticalDistance) + Math.abs(horizontalDistance) + numberOfFreeToChooseMoves;
+};
